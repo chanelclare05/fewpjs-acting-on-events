f94bbe3dd4b2a00b135c09bdee5a1d11670c9ed1
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-23 18:23:21 +1000

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+// select the dodger
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(){
+    let leftNum = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNum, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    let leftNum = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNum, 10);
+    if (left > 0 && left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    } else if (e.key ==='ArrowRight') {
+        moveDodgerRight();
+    }
+})
